595abdebe81a179a2b74729d1fdbf2d667d71e7a
Student <Student@L411-S29>
2018-09-28 12:37:21 +0800

    Completed Problem

--- src/recursionpractice/RecursionPractice.java
+++ src/recursionpractice/RecursionPractice.java
@@ -12,10 +12,25 @@ public class RecursionPractice {
     public static void method(int x, int y){
         method(1, 2);
     }
+    
+    public static double power(double base, int exponent){
+        int product = 1;
+   
+        for(double i = 0; i < exponent; i++){
+            if(i != exponent){
+               product *= base;
+               
+            }
+            
+        }
+        System.out.println("The product is: " + product);
+        return product;
+            
+    }
    
     public static void main(String[] args) {
         
-        
+        power(2, 4);
     }
     
 }
