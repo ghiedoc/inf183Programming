5aaa07ce269a701c76c02a73656253c1252473f4
Student <Student@L411-S29>
2018-09-28 12:45:58 +0800

    Complete

--- src/recursionpractice/RecursionPractice.java
+++ src/recursionpractice/RecursionPractice.java
@@ -1,36 +1,44 @@
-
 package recursionpractice;
 
 public class RecursionPractice {
-    
+
     /**
      * @param x accept first input
-     * @param  y accept second input
-     * 
+     * @param y accept second input
+     *
      * @return sum
      */
-    public static void method(int x, int y){
+    public static void method(int x, int y) {
         method(1, 2);
     }
-    
-    public static double power(double base, int exponent){
+
+    public static double power(double base, int exponent) {
         int product = 1;
-   
-        for(double i = 0; i < exponent; i++){
-            if(i != exponent){
-               product *= base;
-               
+
+        for (double i = 0; i < exponent; i++) {
+            if (i != exponent) {
+                product *= base;
             }
-            
         }
         System.out.println("The product is: " + product);
         return product;
-            
+
     }
-   
+
+    /**
+     *
+     * @param base
+     * @param expo
+     * @return product
+     */
+    public static int powerR(int base, int expo) {
+        int product = 1;
+
+        return product;
+    }
+
     public static void main(String[] args) {
-        
         power(2, 4);
     }
-    
+
 }
