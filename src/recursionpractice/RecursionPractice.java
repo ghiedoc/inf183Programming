859c1cd8d2e09e554c7561479fa24e4a4672459c
Student <Student@L411-S29>
2018-09-28 12:00:19 +0800

    sample of documentation

--- src/recursionpractice/RecursionPractice.java
+++ src/recursionpractice/RecursionPractice.java
@@ -2,10 +2,20 @@
 package recursionpractice;
 
 public class RecursionPractice {
+    
+    /**
+     * @param x accept first input
+     * @param  y accept second input
+     * 
+     * @return sum
+     */
+    public static void method(int x, int y){
+        method(1, 2);
+    }
    
     public static void main(String[] args) {
-        System.out.println("githubbb");
-        System.out.println("push");
+        
+        
     }
     
 }
